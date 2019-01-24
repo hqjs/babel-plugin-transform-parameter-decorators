1cde207bd0e8e8ebf1a199d6e9f6f2965b47c2e5
hqjs <hqjsorg@gmail.com>
2019-01-24 23:35:34 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # babel-plugin-transform-parameter-decorators
+
+# Installation
+```sh
+npm install hqjs@babel-plugin-transform-parameter-decorators
+```
---
+++ index.js
@@ -0,0 +1,38 @@
+export default function({ types }) {
+  return {
+    visitor: {
+      Function: function parseFunctionPath(path) {
+        (path.get('params') || [])
+          .slice()
+          .reverse()
+          .forEach(param => {
+            const { name } = param.node;
+            const { name: paramUidName } = path.scope.generateUidIdentifier(name);
+            let resultantDecorator;
+
+            (param.node.decorators || [])
+              .slice()
+              .reverse()
+              .forEach(decorator => {
+                resultantDecorator = types.callExpression(decorator.expression, [
+                  resultantDecorator || types.Identifier(paramUidName),
+                ]);
+              });
+
+            if (resultantDecorator) {
+              const decoratedParamUidName = path.scope.generateUidIdentifier(name).name;
+
+              path.scope.rename(name, decoratedParamUidName);
+              param.parentPath.get('body').unshiftContainer('body', types.variableDeclaration('var', [
+                types.variableDeclarator(
+                  types.Identifier(decoratedParamUidName),
+                  resultantDecorator
+                ),
+              ]));
+              param.replaceWith(types.Identifier(paramUidName));
+            }
+          });
+      },
+    },
+  };
+}
